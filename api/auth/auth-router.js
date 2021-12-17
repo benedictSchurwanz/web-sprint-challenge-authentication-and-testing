c17dea36340136c86e82f4481a13a009e490f88c
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-12-17 16:53:26 -0600

    login route successful

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -29,8 +29,8 @@ router.post('/login', (req, res, next) => {
       if (user && bcrypt.compareSync(password, user.password)) {// successful login
           const token = tokenBuilder(user)
           res.status(200).json({
-            "message": `welcome, ${user.username}`,
-            "token": [token]
+            message: `welcome, ${user.username}`,
+            token
           })
       } else {
         next({status: 401, message: "invalid credentials"})        
--- data/seeds/01-init.js
+++ data/seeds/01-init.js
@@ -1,7 +1,15 @@
+const bcrypt = require('bcryptjs')
+const { BCRYPT_ROUNDS } = require('../../config')
+
 exports.seed = async function (knex) {
 	await knex('users')
 		.insert({
 			username: 'user',
-			password: '1234',
+			password: bcrypt.hashSync("1234", BCRYPT_ROUNDS),
+		})
+	await knex('users')
+		.insert({
+			username: 'slarty',
+			password: bcrypt.hashSync("bartfast", BCRYPT_ROUNDS),
 		})
 }
